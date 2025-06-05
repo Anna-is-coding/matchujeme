7711188423fa5448e3d1515a785ef14456813696
Vendula Zbořilová <vendulazborilova@gmail.com>
2025-06-06 01:38:45 +0200

    Added Tailwind and Daisy UI, header and homepage edit, responsive navbar in progress

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -5,27 +5,52 @@ import burgerIcon from './icons/burgermenu.svg';
 
 export const Header = () => {
   return (
-    <header>
-      <img className="burgermenu" src={burgerIcon}></img>
-      <div className="nav">
-        <ul>
-          <li>
-            <NavLink to="/">Domů</NavLink>
-          </li>
-          <li>
-            <NavLink to="/facts">Vše o matcha</NavLink>
-          </li>
-          <li>
-            <NavLink to="/quiz">Kvíz</NavLink>
-          </li>
-          <li>
-            <NavLink to="/recipes">Recepty</NavLink>
-          </li>
-          <li>
-            <NavLink to="/stepbystep">Příprava krok za krokem</NavLink>
-          </li>
-        </ul>
+    <header className="shadow-md">
+      <div className="navbar" style={{ backgroundColor: '#3b5704', color: '#fff' }}>
+        {/* MOBILE MENU */}
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li><NavLink to="/">Domů</NavLink></li>
+              <li><NavLink to="/facts">Poznej matchu</NavLink></li>
+              <li><NavLink to="/stepbystep">Příprava</NavLink></li>
+              <li><NavLink to="/recipes">Recepty</NavLink></li>
+              <li><NavLink to="/quiz">Kvíz</NavLink></li>
+            </ul>
+          </div>
+        </div>
+
+        {/* DESKTOP MENU */}
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-vertical lg:menu-horizontal bg-base-1000 rounded-box gap-x-18">
+            <li><NavLink to="/">Domů</NavLink></li>
+            <li><NavLink to="/facts">Poznej matchu</NavLink></li>
+            <li><NavLink to="/stepbystep">Příprava</NavLink></li>
+            <li><NavLink to="/recipes">Recepty</NavLink></li>
+            <li><NavLink to="/quiz">Kvíz</NavLink></li>
+          </ul>
+        </div>
       </div>
     </header>
   );
 };
+
--- src/pages/HomePage/homepage.jsx
+++ src/pages/HomePage/homepage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './homepage.css';
+import homepageImage from './icons/homepage_matcha_pink.jpg';
+
 
 export const HomePage = () => {
   return (
-    <section id="homePage">
-      <div>Ahoj, Matchujeme</div>
-    </section>
+     <div className="hero min-h-screen"
+      style={{
+      backgroundImage: `url(${homepageImage})`,
+      }}>
+      <div className="hero-content text-neutral-content text-center">
+      <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Matchujeme</h1>
+      <p className="mb-8">
+        Zamatchuj se do světa chutí! 
+      </p>
+    </div>
+  </div>
+</div>
   );
 };

f19dbf49430ffb32e7830ff8f66ef8fc026d8f2f
Vendula Zbořilová <vendulazborilova@gmail.com>
2025-06-07 15:01:23 +0200

    Created facts data.js

--- src/pages/Facts/facts.jsx
+++ src/pages/Facts/facts.jsx
@@ -1,27 +1,8 @@
 import React, { useState } from "react";
 import "./facts.css";
 import { MatchaCard } from "../../components/MatchaCard/matchacard";
+import { facts } from "./factsData";
 
-const facts = [
-  {
-    title: "Co je to matcha?",
-    description:
-      "Matcha obsahuje 137x více antioxidantů než běžný zelený čaj!",
-    image: "/img/matcha1.jpg",
-  },
-  {
-    title: "Rituál, ne jen nápoj",
-    description:
-      "Tradiční matcha obřady trvají i přes 30 minut a jsou kulturní událostí.",
-    image: "/img/matcha2.jpg",
-  },
-  {
-    title: "Plná energie bez crashů",
-    description:
-      "Díky L-theaninu tě matcha nakopne, ale zároveň uklidní – win-win!",
-    image: "/img/matcha3.jpg",
-  },
-];
 
 export const Facts = () => {
   const [flipped, setFlipped] = useState(Array(facts.length).fill(false));
---
+++ src/pages/Facts/factsData.js
@@ -0,0 +1,20 @@
+export const facts = [
+  {
+    title: "Co je to matcha?",
+    description:
+      "Matcha obsahuje 137x více antioxidantů než běžný zelený čaj!",
+    image: "/img/matcha1.jpg",
+  },
+  {
+    title: "Rituál, ne jen nápoj",
+    description:
+      "Tradiční matcha obřady trvají i přes 30 minut a jsou kulturní událostí.",
+    image: "/img/matcha2.jpg",
+  },
+  {
+    title: "Plná energie bez crashů",
+    description:
+      "Díky L-theaninu tě matcha nakopne, ale zároveň uklidní – win-win!",
+    image: "/img/matcha3.jpg",
+  },
+];

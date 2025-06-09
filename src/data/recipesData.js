f2ee12b3a2ad9d29f9469e9c533a22e4153c8973
Anna Kršková <krskova.aaa@gmail.com>
2025-06-09 22:28:59 +0200

    Added data for Pink matcha latte recipe

--- src/data/recipesData.js
+++ src/data/recipesData.js
@@ -1,6 +1,49 @@
 export const recipes = [
   {
     id: 1,
+    title: 'Smoothie s matchou',
+    image: '/img/smoothieMatcha.png',
+    serving: 1,
+    ingredients: [
+      '1 banán (ideálně lehce přezrálý)',
+      '1 lžička matchy',
+      '150 ml kokosové vody (nebo mandlového mléka)',
+      '50 g špenátu nebo baby špenátu (volitelné)',
+      '1 lžička medu nebo datlového sirupu (dle chuti)',
+      'hrst ledu',
+    ],
+    instructions: [
+      'Všechny suroviny vlož do mixéru.',
+      'Mixuj do hladka (asi 30 s).',
+      'Nalij do sklenice, ozdob ovocem nebo lístky máty.',
+    ],
+  },
+
+  {
+    id: 2,
+    title: 'Pink matcha latte',
+    image: '/img/pinkMatchaLatte.png',
+    serving: 1,
+    ingredients: [
+      '1 lžička matchy',
+      '60 ml horké vody',
+      '180 ml mléka ',
+      'med nebo oblíbený sirup',
+      'led',
+      '60 ml smetany ke šlehání',
+      '2-3 střední jahody',
+    ],
+    instructions: [
+      'Matcha latte: V malé misce prosej matcha prášek, přilij horkou vodu a metličkou nebo ručním napěňovačem našlehej do pěny. Vmíchej med nebo sirup.',
+      'Do sklenice nasyp led, přilij mléko a matchu, promíchej.',
+      'Jahodová sladká smetana: Jahody rozmixuj do hladka, přidej smetanu, 30 ml mléko a sladidlo.',
+      'Mixuj cca 10 s, dokud krém není jako roztavená zmrzlina.',
+      'Jahodový krém nalij na matcha latte a jemně promíchej, aby vznikl růžovo-zelený gradient.',
+    ],
+  },
+
+  {
+    id: 3,
     title: 'Matcha chia puding',
     image: '/img/chiaPuding.png',
     serving: 2,
@@ -22,40 +65,13 @@ export const recipes = [
 
   {
     id: 2,
-    title: 'Smoothie s matchou',
-    image: '/img/smoothieMatcha.png',
-    serving: 1,
-    ingredients: [
-      '1 banán (ideálně lehce přezrálý)',
-      '1 lžička matchy',
-      '150 ml kokosové vody (nebo mandlového mléka)',
-      '50 g špenátu nebo baby špenátu (volitelné)',
-      '1 lžička medu nebo datlového sirupu (dle chuti)',
-      'hrst ledu',
-    ],
-    instructions: [
-      'Všechny suroviny vlož do mixéru.',
-      'Mixuj do hladka (asi 30 s).',
-      'Nalij do sklenice, ozdob ovocem nebo lístky máty.',
-    ],
-  },
-  {
-    id: 3,
-    title: 'Pink matcha latte',
-    image: '/img/pinkMatchaLatte.png',
-    serving: 1,
-    ingredients: [],
-    instructions: [],
-  },
-  {
-    id: 4,
     title: 'Cold brew matcha',
     image: '/img/coldBrew.png',
     serving: 1,
     ingredients: [
       '1 lžička matchy',
       '300 ml studené vody',
-      '1-2 lžičky agávového sirupu (nebo dle chuti)',
+      '1-2 lžičky agávového sirupu',
       'hrst ledu',
     ],
     instructions: [

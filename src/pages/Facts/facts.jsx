0ca8cc5310b4b0e21f42b2365ab18932f7491fe9
Anna Kršková <krskova.aaa@gmail.com>
2025-06-10 13:18:21 +0200

    deployment troubleshooting - folder name MatchaCard

--- src/pages/Facts/facts.jsx
+++ src/pages/Facts/facts.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './facts.css';
-import { MatchaCard } from '../../components/MatchaCard/matchacard';
+import { MatchaCard } from '../../components/MatchaCard/Matchacard';
 import { facts } from './factsData';
 
 export const Facts = () => {
